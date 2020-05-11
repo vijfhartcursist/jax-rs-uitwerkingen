9eeb01a8c3c1adbcd19d5822d6a4e33e20ba86c2
vijfhartcursist <60903212+vijfhartcursist@users.noreply.github.com>
2020-05-11 14:21:12 +0200

    Add files via upload

---
+++ bookapp/src/main/java/nl/vijfhart/bookapp/BookappRestApplication.java
@@ -0,0 +1,11 @@
+package nl.vijfhart.bookapp;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/bookapp")
+public class BookappRestApplication extends Application {
+}
